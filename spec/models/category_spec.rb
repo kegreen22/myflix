05e5362b153983671df524272c5e5310d767b973
Keith Green <kegreen22@gmail.com>
2016-02-25 18:49:23 -0500

    indented code, clarified variable names

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,24 +1,24 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
 
   it "saves itself" do
-  cat = Category.new(name: "action-comedy")
-  cat.save
-  expect(cat).to eq(cat)
+    cat_init = Category.new(name: "action")
+    cat_init.save
+    expect(cat_init).to eq(cat_init)
   end
- 
+
   it "has valid video" do
     cat = Category.create(name: "action-comedy")
-  k9vid = Video.create(title: "K9", description: "Cop teams with K9 dog", category: "action-comedy")
-   expect(k9vid).to be_valid
+    k9_vid = Video.create(title: "K9", description: "Cop teams with K9 dog", category: "action-comedy")
+    expect(k9vid).to be_valid
   end
 
   it "has many videos" do
-  cat = Category.create(name: "action-comedy")
-  k9vid = Video.create(title: "K9", description: "Cop teams with K9 dog", category: "action-comedy")
-  heatvid = Video.create(title: "The Heat", description: "Two female LEOs team up", category: "action-comedy")
-  expect(action-comedy.videos).to include(k9vid, heatvid)
+    cat = Category.create(name: "action-comedy")
+    k9_vid = Video.create(title: "K9", description: "Cop teams with K9 dog", category: "action-comedy")
+    heat_vid = Video.create(title: "The Heat", description: "Two female LEOs team up", category: "action-comedy")
+    expect(action-comedy.videos).to include(k9_vid, heat_vid)
   end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 
 describe Video do
   it "can save a record" do
-  black_rain = Video.new(title: 'Black Rain', description: 'American cop in Japan', small_cover_url: "/tmp/Batman_Begins.jpg", category_id: '2')
-  black_rain.valid?
-  expect(black_rain).to be_valid
+    black_rain = Video.new(title: 'Black Rain', description: 'American cop in Japan', category: "action", small_cover_url: "/tmp/Batman_Begins.jpg", category_id: '2')
+    black_rain.valid?
+    expect(black_rain).to be_valid
   end
 
   it "belongs to category" do
-  cat = Category.create(name: "action-comedy")
-  taxi = Video.create(title: "taxi", description: "Cop teams with taxi driver", category: "action-comedy")
-  expect(taxi.category).to eq(action-comedy)
+    cat = Category.create(name: "action-comedy")
+    taxi = Video.create(title: "taxi", description: "Cop teams with taxi driver", category: "action-comedy")
+    expect(taxi.category).to eq(action-comedy)
   end
 
   it "is invalid without a title" do
-end 
+  end
 end
